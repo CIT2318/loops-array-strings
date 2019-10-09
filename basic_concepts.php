f57e9a7d45fe8df667cb11d31c2885b72d03b26a
Matthew Mantle <memantle@googlemail.com>
2019-10-09 17:23:50 +0100

    Initial commit

--- basic_concepts.php
+++ basic_concepts.php
@@ -13,9 +13,15 @@ a) Modify the following code so that the loop outputs the numbers 5-15
 b) Re-write the loop as a while loop
 */
 
-for($i=1;$i<=10;$i++)
+for($i=5;$i<=15;$i++)
 {
     echo "{$i}<br>";
+}
+$i=5;
+while($i<=15)
+{
+    echo "{$i}<br>";
+    $i++;
 }
 
 
@@ -29,8 +35,23 @@ http://php.net/manual/en/function.array-merge.php. Output the joined array using
 e) Sort this larger list of countries into reverse alphabetical order (do some research into sorting functions) and output the result using a foreach loop.
 */
 
-//$moreCountries=["Nigeria","Bangladesh","Russia","Japan"];
+$moreCountries=["Nigeria","Bangladesh","Russia","Japan"];
 $countries=["China","India","USA","Indonesia","Brazil","Pakistan"];
+var_dump($countries);
+echo "<p>{$countries[0]}, {$countries[1]}, {$countries[3]} and {$countries[5]} are all in Asia</p>";
+foreach($countries as $country)
+{
+	echo "{$country}<br>";
+}
+$mergedArr = array_merge($countries, $moreCountries);
+var_dump($mergedArr);
+rsort($mergedArr);
+echo "<ul>";
+foreach($mergedArr as $country)
+{
+	echo "<li>{$country}</li>";
+}
+echo "</ul>";
 
 /*
 Q3) Associative Arrays
@@ -43,9 +64,19 @@ d) Output the data from (c) using an HTML table.
 $films=[
     ["title"=>"Jaws", "year"=>"1975", "duration"=>124,"certificate"=>"15"],
     ["title"=>"Spirited Away", "year"=>"2001", "duration"=>124,"certificate"=>"PG"],
-    ["title"=>"Winter's Bone", "year"=>"2010", "duration"=>100,"certificate"=>"15"]
+    ["title"=>"Winter's Bone", "year"=>"2010", "duration"=>100,"certificate"=>"15"],
+    ["title"=>"Do The Right Thing", "year"=>"1989", "duration"=>120,"certificate"=>"15"]
 ];
 
+echo "<p>{$films[1]["title"]} was released in {$films[1]["year"]}</p>";
+echo "<p>{$films[3]["title"]} is {$films[3]["duration"]} minutes long.</p>";
+echo "<table>";
+echo "<tr><th>Title</th><th>Year</th><tr>";
+foreach($films as $film)
+{
+	echo "<tr><td>{$film["title"]}</td><td>{$film["year"]}</td></tr>";
+}
+echo "</table>";
 /*
 Q4) Strings
 a) Using the following string, write an echo statement that outputs the fifth character in the string
@@ -55,7 +86,13 @@ d) Use the substr() (http://php.net/manual/en/function.substr.php) function to o
 */
 
 $moduleStr="CIT2318 Web Design and Programming";
-
+echo "<p>{$moduleStr[4]}</p>";
+$length = strlen($moduleStr);
+echo "<p>String length = {$length}</p>";
+$lowerCase = strtolower($moduleStr);
+echo "<p>{$lowerCase}</p>";
+$substring = substr($moduleStr,8,3);
+echo "<p>{$substring}</p>";
 ?>
 
 </body>
--- display_films.php
+++ display_films.php
@@ -1,10 +1,4 @@
-<?php
-$films=[
-    ["title"=>"Jaws", "year"=>"1975", "duration"=>124,"certificate"=>"15"],
-    ["title"=>"Spirited Away", "year"=>"2001", "duration"=>124,"certificate"=>"PG"],
-    ["title"=>"Winter's Bone", "year"=>"2010", "duration"=>100,"certificate"=>"15"]
-];
-?>
+
 <!DOCTYPE HTML>
 <html>
 <head>
@@ -12,7 +6,24 @@ $films=[
 <meta http-equiv="content-type" content="text/html;charset=utf-8" />
 </head>
 <body>
-Search results will appear here.
+<?php
+  $films=[
+      ["title"=>"Jaws", "year"=>"1975", "duration"=>124,"certificate"=>"15"],
+      ["title"=>"Spirited Away", "year"=>"2001", "duration"=>124,"certificate"=>"PG"],
+      ["title"=>"Winter's Bone", "year"=>"2010", "duration"=>100,"certificate"=>"15"],
+      ["title"=>"The Godfather", "year"=>"1972", "duration"=>177,"certificate"=>"18"]
+  ];
+  if(isset($_POST["submitBtn"])){
+    $certificate=$_POST["certificate"];
+    foreach($films as $film){
+      if($film["certificate"]===$certificate){
+        echo "<p>{$film["title"]}</p>";
+      }
+    }
+  }else{
+    echo "<p>You shouldn't have got to this page</p>";
+  }
+?>
 
 </body>
 </html>
